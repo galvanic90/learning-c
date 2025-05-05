c6a96e184b2f1c5de968c80765842d43ef9d68f9
xaraxx <sara.galvan@udea.edu.co>
2025-05-05 11:47:06 -0400

    this codes corespond toprogramming excercises in the chapter 2 of the Deitel book

---
+++ chap_2/print.c
@@ -0,0 +1,13 @@
+/* Mi primer programa en C */
+
+#include <stdio.h>
+
+/*This function starts the program execution */
+/* En este archivo he cometido un error muy común que es nombrar el archivo con el nombre de la función que estoy utilizando, en este caso print. */
+int main(void)
+{
+	printf("Bienvenido a C!\n");
+	printf("Este es mi primer programa en C");
+
+	return 0;
+}
---
+++ chap_2/suma.c
@@ -0,0 +1,14 @@
+/* Este programa calcula la suma de dos números decimales */
+
+#include <stdio.h>
+
+int main(void)
+{
+   printf("Este programa realiza la suma de dos números decimales e imprime \n el resultado en la pantalla\n");
+   int entero1; /*primer número a introducir por el usuario*/
+   int entero2;
+   printf("Introduzca el primer número a sumar \n");
+   scanf("%d", &entero1);
+
+   return 0;
+}
