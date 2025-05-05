8fb6020ac8c5e673e1322482c27eec85d2a37bc6
xaraxx <sara.galvan@udea.edu.co>
2025-05-05 12:17:13 -0400

    making a code that sumarize 2 numbers

--- chap_2/suma.c
+++ chap_2/suma.c
@@ -7,8 +7,15 @@ int main(void)
    printf("Este programa realiza la suma de dos números decimales e imprime \n el resultado en la pantalla\n");
    int entero1; /*primer número a introducir por el usuario*/
    int entero2;
-   printf("Introduzca el primer número a sumar \n");
+   
+   printf("Introduzca el primer número a sumar: \n");
    scanf("%d", &entero1);
+
+   printf("Introduzca el segundo número a sumar: \n");
+   scanf("%d", &entero2);
+
+   int resultado = entero1 + entero2;
+   printf("La suma de estos números es\n%d \n", resultado);	
 
    return 0;
 }
